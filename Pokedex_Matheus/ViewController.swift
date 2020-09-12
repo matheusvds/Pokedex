6617a60a9b50cf427ba3b9e096023636bc944510
Matheus Sousa <matheusvds@gmail.com>
2020-09-12 16:44:19 -0300

    adds initial project

--- Pokedex_Matheus/ViewController.swift
+++ Pokedex_Matheus/ViewController.swift
@@ -1,20 +1,6 @@
-//
-//  ViewController.swift
-//  Pokedex_Matheus
-//
-//  Created by Matheus Vasconcelos de Sousa on 12/09/20.
-//  Copyright © 2020 Matheus Vasconcelos de Sousa. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    
 }
 
