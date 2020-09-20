d143c734cc0a5745ba07baa3e149756c6e9b39c9
Matheus Sousa <matheusvds@gmail.com>
2020-09-20 16:07:14 -0300

    adds test failing message for better understanding CI error

--- InfraTests/URLSessionAdapterTests.swift
+++ InfraTests/URLSessionAdapterTests.swift
@@ -13,7 +13,7 @@ class URLSessionAdapterTests: XCTestCase {
     
     func test_get_should_have_request_with_valid_method() {
         testRequest { (request) in
-            XCTAssertEqual("GET", request.httpMethod)
+            XCTAssertEqual("GET", request.httpMethod, "expecting GET, got \(request.httpMethod ?? "no request")")
         }
     }
     
